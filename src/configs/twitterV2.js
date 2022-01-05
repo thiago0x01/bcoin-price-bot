ecd7c0eec0bd95ac03c5de13ca106ccb97433fdf
Thiago Machado <thcosta0@hotmail.com>
2022-01-05 19:01:00 -0300

    oauth1 request fix

--- src/configs/twitterV2.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const twitterV2 = axios.create({
-  baseURL: 'https://api.twitter.com/2'
-});
-
-export default twitterV2;
--- src/controllers/createATweetController.js
+++ src/controllers/createATweetController.js
@@ -1,32 +1,23 @@
+import { TwitterClient } from 'twitter-api-client';
 import currencyConverterAPI from '../configs/currencyConverterAPI.js';
-import twitterV2 from '../configs/twitterV2.js';
 import { getPrice, getPriceShift } from '../tokens/BCOIN.js';
-import { convertCurrency } from '../tools/index.js';
 
 async function createATweetController() {
   const brlPrice = await getPrice();
   const priceShift = await getPriceShift();
   const brlToUsdAndBrlToEur = await currencyConverterAPI.get('/');
 
+  const twitterClient = new TwitterClient({
+    apiKey: process.env.TWITTER_CONSUMER_KEY,
+    apiSecret: process.env.TWITTER_CONSUMER_SECRET,
+    accessToken: process.env.TWITTER_ACCESS_TOKEN,
+    accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  });
+
   try {
-    await twitterV2.post(
-      '/tweets',
-      {
-        text: `🇧🇷\nPreço de Bombcrypto (BCOIN): ${brlPrice}\nAlternância de Preço (24h): ${priceShift}\n\n🇺🇸\nBombcrypto Price (BCOIN): $${convertCurrency(
-          brlToUsdAndBrlToEur.data.BRL_USD,
-          brlPrice
-        )}\nPrice Shift (24h): ${priceShift}\n\n🇪🇸\nPrecio de Bombcrypto (BCOIN): €${convertCurrency(
-          brlToUsdAndBrlToEur.data.BRL_EUR,
-          brlPrice
-        )}\nCambio de precio (24h): ${priceShift}`
-      },
-      {
-        headers: {
-          Authorization: `OAuth oauth_consumer_key="${process.env.TWITTER_CONSUMER_KEY}",oauth_token="${process.env.TWITTER_ACCESS_TOKEN}",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1641313593",oauth_nonce="pN9OPNFvMdF",oauth_version="1.0",oauth_signature="sIqmMRfXFVq%2BpuUMmG08z%2Bc0RwM%3D"`,
-          'Content-Type': 'application/json'
-        }
-      }
-    );
+    await twitterClient.tweetsV2.createTweet({
+      text: `I'm back`
+    });
   } catch (error) {
     console.log(error);
   }
