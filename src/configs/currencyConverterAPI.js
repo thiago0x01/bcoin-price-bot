0c3e7f4edd9b6c2ad5328ade61afd8ac1da3993d
Thiago Machado <thcosta0@hotmail.com>
2022-01-05 19:05:54 -0300

    finally

--- src/configs/currencyConverterAPI.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const currencyConverterAPI = axios.create({
-  baseURL:
-    'https://free.currconv.com/api/v7/convert?q=BRL_USD,BRL_EUR&compact=ultra&apiKey=270327f79dfab90b113c'
-});
-
-export default currencyConverterAPI;
--- src/controllers/createATweetController.js
+++ src/controllers/createATweetController.js
@@ -1,11 +1,13 @@
+import axios from 'axios';
 import { TwitterClient } from 'twitter-api-client';
-import currencyConverterAPI from '../configs/currencyConverterAPI.js';
 import { getPrice, getPriceShift } from '../tokens/BCOIN.js';
 
 async function createATweetController() {
   const brlPrice = await getPrice();
   const priceShift = await getPriceShift();
-  const brlToUsdAndBrlToEur = await currencyConverterAPI.get('/');
+  const brlToUsdAndBrlToEur = await axios.get(
+    'https://free.currconv.com/api/v7/convert?q=BRL_USD,BRL_EUR&compact=ultra&apiKey=270327f79dfab90b113c'
+  );
 
   const twitterClient = new TwitterClient({
     apiKey: process.env.TWITTER_CONSUMER_KEY,
@@ -16,7 +18,13 @@ async function createATweetController() {
 
   try {
     await twitterClient.tweetsV2.createTweet({
-      text: `I'm back`
+      text: `🇧🇷\nPreço de Bombcrypto (BCOIN): ${brlPrice}\nAlternância de Preço (24h): ${priceShift}\n\n🇺🇸\nBombcrypto Price (BCOIN): $${convertCurrency(
+        brlToUsdAndBrlToEur.data.BRL_USD,
+        brlPrice
+      )}\nPrice Shift (24h): ${priceShift}\n\n🇪🇸\nPrecio de Bombcrypto (BCOIN): €${convertCurrency(
+        brlToUsdAndBrlToEur.data.BRL_EUR,
+        brlPrice
+      )}\nCambio de precio (24h): ${priceShift}`
     });
   } catch (error) {
     console.log(error);
